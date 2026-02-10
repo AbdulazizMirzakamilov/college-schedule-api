2528443cfa5e4da0108b0be01ad245e7dbbbe6ca
Abdulaziz <alikom904@gmail.com>
2026-02-10 16:13:07 +0700

    Add schedule API controller

--- CollegeSchedule/Program.cs
+++ CollegeSchedule/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<IScheduleService, ScheduleService>();
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
