866e3721ee4a8ca503e1f6db07717d9a6d5d6d4d
Abdulaziz <alikom904@gmail.com>
2026-02-10 15:46:59 +0700

    Add DTOs for schedule API responses

--- CollegeSchedule/Program.cs
+++ CollegeSchedule/Program.cs
@@ -1,8 +1,25 @@
+using CollegeSchedule.Data;
+//using CollegeSchedule.Middlewares;
+using CollegeSchedule.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+DotNetEnv.Env.Load();
 
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                       $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                       $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+                       $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+                       $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IScheduleService, ScheduleService>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,8 +34,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+//app.UseMiddleware<ExceptionMiddleware>();
 app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
